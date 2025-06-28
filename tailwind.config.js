6f47ab060102da6a6d0405ff8376464021d9ab79
Bruno Marshall <35432495+BrunoMarshall@users.noreply.github.com>
2025-06-29 00:45:34 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'vendly-blue': '#1E3A8A',
+        'vendly-accent': '#F59E0B',
+      },
+    },
+  },
+  plugins: [],
+};

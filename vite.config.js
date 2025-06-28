ad591aaf1e2cf89bfe8f9ea9e1c5ffb53242384f
Bruno Marshall <35432495+BrunoMarshall@users.noreply.github.com>
2025-06-29 01:28:40 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set to '/web3vendly' if deploying to a subdirectory
+  base: '/',
 });
